a126f6ea742c94cacb06fa780a5941c7c8485267
kirdanic <90120044+kirdanic@users.noreply.github.com>
2022-08-16 15:57:04 -0400

    first commit message

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+import { useLocation } from 'react-router-dom'
+
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation()
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      {location.pathname === '/' && (
+      <Button color={showAdd ? 
+      'red' : 'skyblue'} 
+      text={showAdd ? 
+      '-' : '+'} 
+      onClick={onAdd} />)}
+    </header>
+  )
+}
+
+Header.defaultProps = {
+    title: 'Taskey',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+export default Header
